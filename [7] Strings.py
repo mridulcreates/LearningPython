53d841d626b028c76d4dbd8fbb63afe92218d8aa
Mridul <mridulbud@gmail.com>
2025-08-28 22:05:09 +0530

    Casting and strings

---
+++ [7] Strings.py
@@ -0,0 +1,72 @@
+# PRINT A STRING
+
+a = "Hello"
+print(a)
+
+# STRINGS ARE ARRAYS
+
+a = "Hello world"
+print(a[0])
+
+# LOOPING THROUGH A STRING
+
+for i in "banana":
+    print(i)
+
+# STRING LENGTH
+
+a = "Hello World"
+print(len(a))
+
+# CHECKING A STRING
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+if "free" not in txt:
+   print("No, free is not present.")
+
+# SLICING A STRING
+
+a = "Hello, World!"
+print(a[2:4]) # This will print characters from index 2 to 4 (not including 4)
+
+# -----------------------------------------------
+
+a = "Hello, World!"
+print(a[:4])
+
+# -----------------------------------------------
+
+a = "Hello, World!"
+print(a[2:])
+
+# -----------------------------------------------
+
+a = "Hello, World!"
+print(a[-5:-2])
+
+# MODIFYING A STRING
+
+a = "Hello, World!"
+print(a.upper()) # This will convert the string to upper case
+
+# -----------------------------------------------
+
+a = "Hello, World!"
+print(a.lower()) # This will convert the string to lower case
+
+# -----------------------------------------------
+
+a = " Hello, World! "
+print(a.strip()) # This will remove any whitespace from the beginning or the end
+
+# -----------------------------------------------
+
+a = "Hello, World!"
+print(a.replace("H", "J")) # This will replace H with J
+
+# -----------------------------------------------
+
+a = "Hello, World!"
+print(a.split(",")) # This will split the string into a list where there is a comma
