19287122e334244d63e69564c0f456c5f7b9a5c6
Mridul <mridulbud@gmail.com>
2025-08-28 20:45:01 +0530

    Data type

---
+++ Data Types.py
@@ -0,0 +1,13 @@
+"""
+Text type: str
+Numeric types: int, float, complex
+Sequence types: list, tuple, range
+Mapping type: dict
+Set types: set, frozenset
+Boolean type: bool
+Binary types: bytes, bytearray, memoryview
+None type: NoneType
+"""
+
+x = 5
+print(type(x))
